8bac1af14fcf96da9268753e02e258dd58a7786a
upandya999 <umpandya999@gmail.com>
2026-03-10 19:18:44 -0500

    made the changes

--- airflow/dags/ecommerece_pipeline.py
+++ airflow/dags/ecommerece_pipeline.py
@@ -6,7 +6,7 @@ from ingestion.ingest_products import fetch_products
 from ingestion.ingest_orders import generate_orders
 from transformations.transform_orders import transform_orders
 from data_quality.validate_orders import validate_orders
-
+# this is to add a line
 with DAG(
     dag_id="ecommerce_pipeline",
     start_date=datetime(2024,1,1),
